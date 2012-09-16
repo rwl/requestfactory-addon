c5fe9a6c82011d0842635359162edaf0d3c5a4f4
rwl <r.w.lincoln@gmail.com>
2012-09-16 14:32:48 +0200

    Using Gwt Bootstrap buttons in edit and details views.

--- addon-gwt/src/main/resources/org/springframework/roo/addon/gwt/module/client/scaffold/place/AbstractProxyListView-template.java
+++ addon-gwt/src/main/resources/org/springframework/roo/addon/gwt/module/client/scaffold/place/AbstractProxyListView-template.java
@@ -1,9 +1,9 @@
 package __TOP_LEVEL_PACKAGE__.client.scaffold.place;
 
+import com.github.gwtbootstrap.client.ui.Button;
 import com.google.gwt.event.dom.client.ClickEvent;
 import com.google.gwt.event.dom.client.ClickHandler;
 import com.google.web.bindery.requestfactory.shared.EntityProxy;
-import com.google.gwt.user.client.ui.Button;
 import com.google.gwt.user.client.ui.Composite;
 import com.google.gwt.user.client.ui.Widget;
 import com.google.gwt.view.client.HasData;
